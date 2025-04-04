e263cee36fa6b0f069299f36cee0903f1be9eb82
“Aiden <adnrdmnd@gmail.com>
2025-04-04 10:54:46 -0400

    dark/light mode

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,40 @@
 <script setup>
 import { ref } from "vue";
-import { invoke } from "@tauri-apps/api/core";
+const buttonText = ref("dark mode");
 
-const greetMsg = ref("");
-const name = ref("");
+function changeMode() {
+    if (buttonText.value == "dark mode") {
+        buttonText.value = "light mode";
+    } else {
+        buttonText.value = "dark mode";
+    }
 
-async function greet() {
-    // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-    greetMsg.value = await invoke("greet", { name: name.value });
+    let root = document.getElementById("root");
+
+    if (root.className == "dark") {
+        root.className = "light";
+    } else {
+        root.className = "dark";
+    }
 }
 </script>
 
 <template>
     <div class="flex flex-col flex-1 items-center justify-center">
-        <h1>hello there</h1>
+        <div class="flex gap-4 mb-12 p-4 bg-black/25 dark:bg-white/25">
+            <div class="p-12 rounded-full bg-black dark:bg-black-dark"></div>
+            <div class="p-12 rounded-full bg-white dark:bg-white-dark"></div>
+            <div
+                class="p-12 rounded-full bg-primary dark:bg-primary-dark"
+            ></div>
+            <div
+                class="p-12 rounded-full bg-secondary dark:bg-secondary-dark"
+            ></div>
+            <div class="p-12 rounded-full bg-accent dark:bg-accent-dark"></div>
+        </div>
+
+        <button id="dark-mode-button" @click="changeMode">
+            {{ buttonText }}
+        </button>
     </div>
 </template>
-
-<!--         <h1>Welcome to Tauri + Vue</h1>
-
-<form class="row" @submit.prevent="greet">
-    <input
-        id="greet-input"
-        v-model="name"
-        placeholder="Enter a name..."
-    />
-    <button type="submit">Greet</button>
-</form>
-<p>{{ greetMsg }}</p> -->
