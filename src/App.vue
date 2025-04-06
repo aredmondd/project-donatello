994273f11eba5e9f9db91ad75f69252210f21801
Aiden Redmond <adnrdmnd@gmail.com>
2025-04-06 17:38:16 -0400

    custom font

--- src/App.vue
+++ src/App.vue
@@ -22,14 +22,14 @@ function changeMode() {
 <template>
     <div class="flex flex-col flex-1 items-center justify-center">
         <div class="flex gap-4 mb-12 p-4">
-            <div class="p-12 rounded-full bg-white border-1 border-black dark:bg-black dark:border-white"></div>
+            <div class="p-12 rounded-full bg-white border-1 dark:bg-black"></div>
             <div class="p-12 rounded-full bg-black dark:bg-white"></div>
             <div class="p-12 rounded-full bg-primary dark:bg-secondary"></div>
             <div class="p-12 rounded-full bg-secondary dark:bg-primary"></div>
             <div class="p-12 rounded-full bg-accent"></div>
         </div>
 
-        <button id="dark-mode-button" @click="changeMode">
+        <button id="dark-mode-button" @click="changeMode" class="font-daydream border-2 p-4 rounded-full">
             {{ buttonText }}
         </button>
     </div>
